47db4fc1f58bb2e57dc8bcf44cf0d76d20fb22ad
Linus Heckemann <git@sphalerite.org>
2022-06-13 13:46:49 +0200

    remove noise from nix expression

--- package.nix
+++ package.nix
@@ -1,33 +1,14 @@
 { buildPythonPackage
 , lib
 , nix-gitignore
-, flask
-, flask_login
-, flask_sqlalchemy
-, flask_migrate
-, flask_wtf
-, flask-talisman
-, email_validator
-, requests
-, py_scrypt
-, sqlalchemy-continuum
-, feedgen
-, pytz
-, authlib
-, markupsafe
-, pacman
-, pkg-config
-, libarchive
-, git
 , poetry2nix
 , openssl
 }:
 poetry2nix.mkPoetryApplication {
-  #pname = "arch-security-tracker";
-  #version = "unstable";
   projectDir = ./.;
   src = nix-gitignore.gitignoreSource [
     "*.nix"
+    "result"
   ] ./.;
 
   overrides = poetry2nix.overrides.withDefaults (self: super: {
@@ -35,20 +16,4 @@ poetry2nix.mkPoetryApplication {
       buildInputs = o.buildInputs ++ [openssl];
     });
   });
-  #buildInputs = [
-  #  flask
-  #  flask_login
-  #  flask_sqlalchemy
-  #  flask_migrate
-  #  flask_wtf
-  #  flask-talisman
-  #  email_validator
-  #  requests
-  #  py_scrypt
-  #  sqlalchemy-continuum
-  #  feedgen
-  #  pytz
-  #  authlib
-  #  markupsafe
-  #];
 }
