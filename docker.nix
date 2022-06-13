cbff31492954641e888f1f491e0587e8c64a2c63
Linus Heckemann <git@sphalerite.org>
2022-06-13 14:51:41 +0200

    More metadata for docker image

--- docker.nix
+++ docker.nix
@@ -1,9 +1,11 @@
 { arch-security-tracker, dockerTools }:
 dockerTools.buildLayeredImage {
   name = "security-tracker";
+  tag = arch-security-tracker.version;
   contents = [ arch-security-tracker ];
   config = {
     Volumes."/var/lib/security-tracker" = {};
-    Cmd = ["gunicorn" "tracker:app"];
+    Cmd = ["gunicorn" "tracker:app" "-b" "[::]:8000"];
+    ExposedPorts."8000" = {};
   };
 }
