1d72cfbef774e59cf488db8139c4a323ec9d8b20
Linus Heckemann <git@sphalerite.org>
2022-06-17 18:20:27 +0000

    flake, nixos module, nixos test

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS security tracker";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    nixosModule = { system, lib, ... }: {
+      imports = [ ./module.nix ];
+      # TODO: decide if this makes sense
+      #security-tracker.package = lib.mkDefault self.defaultPackage.${system};
+    };
+  } // flake-utils.lib.eachDefaultSystem (system: {
+    packages.security-tracker = nixpkgs.legacyPackages.${system}.callPackage ./package.nix {};
+    defaultPackage = self.packages.${system}.security-tracker;
+
+    nixosTest = nixpkgs.legacyPackages.${system}.nixosTest {
+      nodes.machine = {
+        imports = [ self.nixosModule ];
+        security-tracker.enable = true;
+      };
+
+      testScript = ''
+        machine.wait_for_unit("multi-user.target")
+        machine.succeed("trackerctl db initdb")
+        machine.wait_until_succeeds("curl --silent --show-error --fail localhost:8000")
+      '';
+    };
+  });
+}
---
+++ module.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.security-tracker;
+  settingsFormat = pkgs.formats.ini {};
+in {
+  options.security-tracker = {
+    enable = lib.mkEnableOption "NixOS security tracker";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.callPackage ./package.nix {};
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 8000;
+    };
+    settings = lib.mkOption {
+      description = ''
+        Settings for the security tracker, which override the defaults from the package.
+
+        See https://github.com/DeterminateSystems/security-tracker/blob/master/config/00-default.conf for the defaults.
+      '';
+      type = settingsFormat.type;
+      default = {};
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    systemd.services.security-tracker = {
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        ExecStart = "${cfg.package.dependencyEnv}/bin/gunicorn tracker:app [::]:${toString cfg.port}";
+        DynamicUser = true;
+        StateDirectory = "security-tracker";
+      };
+    };
+    environment.etc."security-tracker/nixos.conf".source = settingsFormat.generate "security-tracker-nixos.conf" cfg.settings;
+    # Is this a sensible approach to getting scripts to run in the DynamicUser context?
+    environment.systemPackages = [ (pkgs.writeScriptBin "trackerctl" ''
+      #!${pkgs.runtimeShell}
+      stdio_flag=--pipe
+      [[ -t 0 ]] && stdio_flag=--pty
+      exec systemd-run \
+        -p User=security-tracker \
+        -p DynamicUser=true \
+        -p StateDirectory=security-tracker \
+        "$stdio_flag" \
+        -- ${cfg.package}/bin/trackerctl "$@"
+    '') ];
+  };
+}
